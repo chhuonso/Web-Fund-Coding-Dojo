48894b815dfbacc632c98fc46ec1ddf03b5d45cd
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-06-24 23:23:50 -0700

    Profile Page assignment

--- week1/wk1day3/algo.js
+++ week1/wk1day3/algo.js
@@ -13,3 +13,55 @@ console.log(result1);
     
 var result2 = isPal( [3, 2, 1, 1, 2, 3] );
 console.log(result2)
+
+
+
+
+function reverse(arr) {
+    for(var left=0; left<arr.length/2; left++) {
+        var right = arr.length-1-left;
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        }
+        return arr ;
+    }
+
+    
+var result1 = reverse( ["a", "b", "c", "d", "e"] );
+console.log(result1);
+    
+var result2 = reverse( [3, 2, 1, 1, 2, 3] );
+console.log(result2)
+
+var number = 1;
+var letters = ["A", "B", "C", "D", "E"]
+function compute(){
+    for(var i = 0; i < letters.length; i++){
+        if(i == 1){
+            number = number + number * number
+            return number + letters[i]
+        }
+    }
+    return number + letters[i]
+}
+var answer = compute();
+console.log(answer);
+
+
+function Something(arr){
+    var result = 0;
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] > 10){
+            console.log("We got a big one!");
+        } else {
+            result+=arr[i]
+        }
+    }
+    if(result < 10){
+        console.log("You're killin' me, Smalls");
+    } else {
+        return "You're gonna need a bigger funcion!";
+    }
+}
+Something([1,8,21,-11,12,2,9,15,7]);
