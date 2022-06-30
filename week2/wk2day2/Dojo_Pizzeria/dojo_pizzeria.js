b4093ff106c433c5d5ce61cd191eea11d59939bb
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-06-29 19:05:29 -0700

    pizzzeria  and bonus example

--- week2/wk2day2/Dojo_Pizzeria/dojo_pizzeria.js
+++ week2/wk2day2/Dojo_Pizzeria/dojo_pizzeria.js
@@ -1,17 +1,100 @@
+//function setup with elemens as key 
+
 function pizzaOven(crustType, sauceType, cheeses, toppings){
     var pizza = {}
-        pizza.crustType = "deep dish";
-        pizza.sauceType = "traditinal";
-        pizza.cheeses = ["mozzarella"];
-        pizza.toppings = ["pepperoni", "sausage"];
+        pizza.crustType = crustType;
+        pizza.sauceType = sauceType;
+        pizza.cheeses = cheeses;
+        pizza.toppings = toppings;
         return pizza;
     }
 
 
-var pizza1 = pizzaOven(
-    "hand tossed", 
-    "marinara", 
-    ["mozzarella", "feta"],
-    ["mushrooms", "olives", "onions"])
+var pizza1 = pizzaOven("deep dish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
+console.log("**************** PIZZA 1 *********************")
+console.log(pizza1)
+var pizza2 = pizzaOven("thin crust", "marinara", ["mozzarella", "feta", "cheddar"], ["mushrooms", "onions"]);
+console.log("**************** PIZZA 2 *********************")
+console.log(pizza2)
+var pizza3 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives"]);
+console.log("**************** PIZZA 3 *********************")
+console.log(pizza3)
+var pizza4 = pizzaOven("deep crust", "marinara", ["mozzarella", "cheddar"], [ "olives", "onions"]);
+console.log("**************** PIZZA 4 *********************")
+console.log(pizza4)
 
 
+
+
+
+
+
+// BONUS EXAMPLE from video
+
+var crustTypes = [
+    "deep dish",
+    "hand tossed",
+    "thin and crispy",
+    "cauliflower",
+    "gluten free",
+    "hawaiian bread"
+];
+
+var sauceTypes = [
+    "traditional",
+    "marinara",
+    "bbq",
+    "white sauce",
+    "nacho cheese",
+    "tikka masala"
+];
+
+var cheeses = [
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "swiss cheese",
+    "blue cheese",
+    "goat cheese",
+    "provolone",
+    "parmesan",
+    "vegan cheese"
+];
+
+var toppings = [
+    "pepperoni",
+    "sausage",
+    "chicken",
+    "corn",
+    "olives",
+    "bell peppers",
+    "onions",
+    "mushrooms",
+    "anchovies"
+];
+
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPick(arr) {
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = randomPick(crustTypes);
+    pizza.sauceType = randomPick(sauceTypes);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<randomRange(5, 1); i++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for(var i=0; i<randomRange(4, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+console.log(randomPizza());
