c5c0b2e5e0b6c60179186584126de59dc3c6873b
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-06-29 17:28:13 -0700

    fixed to one function for all like button

--- week2/wk2day2/LIkes/likes.js
+++ week2/wk2day2/LIkes/likes.js
@@ -2,11 +2,3 @@ function likeUpdate1(element){
     var liked = document.querySelector(element);
     liked.innerText++
 }
-function likeUpdate2(element){
-    var liked = document.querySelector(element);
-    liked.innerText++
-}
-function likeUpdate3(element){
-    var liked = document.querySelector(element);
-    liked.innerText++
-}
