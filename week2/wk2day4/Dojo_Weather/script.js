41dcaca02950d92c32a9573df3718b665fcb326e
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-06-30 21:08:51 -0700

    Dojo Weather

---
+++ week2/wk2day4/Dojo_Weather/script.js
@@ -0,0 +1,32 @@
+function displayAlert(){
+    alert("Loading weather report...")
+}
+
+
+function accept(element){
+    var cookie = document.querySelector(element);
+    cookie.remove();
+}
+
+
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
+}
+
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
+
+function convert(element) {
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = f2c(tempVal);
+        } else {
+            tempSpan.innerText = c2f(tempVal);
+        }
+    }
+}
+
+
